9f6a8dc90acc5ef94360f43317530e2aa47a829f
Shujaa Ahmad <shujaa25@gmail.com>
2022-04-05 16:29:22 +0530

    initial commit

---
+++ popup.js
@@ -0,0 +1,108 @@
+
+var symCodes = [{"symbol":"$","codes":["usd","cad","ars","aud","bnd","brl","bzd","clp","cop","cve","dop","hkd","jmd","mop","mxn","nad","nio","nzd","sgd","top","ttd","twd","uyu","zwl"],"names":["US DOLLAR","CANADIAN DOLLAR","ARGENTINE PESO","AUSTRALIAN DOLLAR","BRUNEI DOLLAR","BRAZILIAN REAL","BELIZE DOLLAR","CHILEAN PESO","COLOMBIAN PESO","CAPE VERDEAN ESCUDO","DOMINICAN PESO","HONG KONG DOLLAR","JAMAICAN DOLLAR","MACANESE PATACA","MEXICAN PESO","NAMIBIAN DOLLAR","NICARAGUAN CÓRDOBA","NEW ZEALAND DOLLAR","SINGAPORE DOLLAR","TONGAN PAʻANGA","TRINIDAD AND TOBAGO DOLLAR","NEW TAIWAN DOLLAR","URUGUAYAN PESO","ZIMBABWEAN DOLLAR"]},{"symbol":"CA$","codes":["cad"],"names":["CANADIAN DOLLAR"]},{"symbol":"€","codes":["eur"],"names":["EURO"]},{"symbol":"AED","codes":["aed"],"names":["UNITED ARAB EMIRATES DIRHAM"]},{"symbol":"Af","codes":["afn"],"names":["AFGHAN AFGHANI"]},{"symbol":"ALL","codes":["all"],"names":["ALBANIAN LEK"]},{"symbol":"AMD","codes":["amd"],"names":["ARMENIAN DRAM"]},{"symbol":"AR$","codes":["ars"],"names":["ARGENTINE PESO"]},{"symbol":"AU$","codes":["aud"],"names":["AUSTRALIAN DOLLAR"]},{"symbol":"man.","codes":["azn"],"names":["AZERBAIJANI MANAT"]},{"symbol":"KM","codes":["bam"],"names":["BOSNIA-HERZEGOVINA CONVERTIBLE MARK"]},{"symbol":"Tk","codes":["bdt"],"names":["BANGLADESHI TAKA"]},{"symbol":"BGN","codes":["bgn"],"names":["BULGARIAN LEV"]},{"symbol":"BD","codes":["bhd"],"names":["BAHRAINI DINAR"]},{"symbol":"FBu","codes":["bif"],"names":["BURUNDIAN FRANC"]},{"symbol":"BN$","codes":["bnd"],"names":["BRUNEI DOLLAR"]},{"symbol":"Bs","codes":["bob","vef"],"names":["BOLIVIAN BOLIVIANO","VENEZUELAN BOLÍVAR"]},{"symbol":"R$","codes":["brl"],"names":["BRAZILIAN REAL"]},{"symbol":"BWP","codes":["bwp"],"names":["BOTSWANAN PULA"]},{"symbol":"Br","codes":["byn","etb"],"names":["BELARUSIAN RUBLE","ETHIOPIAN BIRR"]},{"symbol":"BZ$","codes":["bzd"],"names":["BELIZE DOLLAR"]},{"symbol":"CDF","codes":["cdf"],"names":["CONGOLESE FRANC"]},{"symbol":"CHF","codes":["chf"],"names":["SWISS FRANC"]},{"symbol":"CL$","codes":["clp"],"names":["CHILEAN PESO"]},{"symbol":"CN¥","codes":["cny"],"names":["CHINESE YUAN"]},{"symbol":"CO$","codes":["cop"],"names":["COLOMBIAN PESO"]},{"symbol":"₡","codes":["crc"],"names":["COSTA RICAN COLÓN"]},{"symbol":"CV$","codes":["cve"],"names":["CAPE VERDEAN ESCUDO"]},{"symbol":"Kč","codes":["czk"],"names":["CZECH REPUBLIC KORUNA"]},{"symbol":"Fdj","codes":["djf"],"names":["DJIBOUTIAN FRANC"]},{"symbol":"Dkr","codes":["dkk"],"names":["DANISH KRONE"]},{"symbol":"RD$","codes":["dop"],"names":["DOMINICAN PESO"]},{"symbol":"DA","codes":["dzd"],"names":["ALGERIAN DINAR"]},{"symbol":"Ekr","codes":["eek"],"names":["ESTONIAN KROON"]},{"symbol":"EGP","codes":["egp"],"names":["EGYPTIAN POUND"]},{"symbol":"Nfk","codes":["ern"],"names":["ERITREAN NAKFA"]},{"symbol":"Br","codes":["etb"],"names":["ETHIOPIAN BIRR"]},{"symbol":"£","codes":["gbp","syp"],"names":["BRITISH POUND STERLING","SYRIAN POUND"]},{"symbol":"GEL","codes":["gel"],"names":["GEORGIAN LARI"]},{"symbol":"GH₵","codes":["ghs"],"names":["GHANAIAN CEDI"]},{"symbol":"FG","codes":["gnf"],"names":["GUINEAN FRANC"]},{"symbol":"GTQ","codes":["gtq"],"names":["GUATEMALAN QUETZAL"]},{"symbol":"HK$","codes":["hkd"],"names":["HONG KONG DOLLAR"]},{"symbol":"HNL","codes":["hnl"],"names":["HONDURAN LEMPIRA"]},{"symbol":"kn","codes":["hrk"],"names":["CROATIAN KUNA"]},{"symbol":"Ft","codes":["huf"],"names":["HUNGARIAN FORINT"]},{"symbol":"Rp","codes":["idr"],"names":["INDONESIAN RUPIAH"]},{"symbol":"₪","codes":["ils"],"names":["ISRAELI NEW SHEQEL"]},{"symbol":"₹","codes":["inr"],"names":["INDIAN RUPEE"]},{"symbol":"Rs","codes":["inr","lkr","mur","npr","pkr"],"names":["INDIAN RUPEE","SRI LANKAN RUPEE","MAURITIAN RUPEE","NEPALESE RUPEE","PAKISTANI RUPEE"]},{"symbol":"IQD","codes":["iqd"],"names":["IRAQI DINAR"]},{"symbol":"IRR","codes":["irr"],"names":["IRANIAN RIAL"]},{"symbol":"Ikr","codes":["isk"],"names":["ICELANDIC KRÓNA"]},{"symbol":"J$","codes":["jmd"],"names":["JAMAICAN DOLLAR"]},{"symbol":"JD","codes":["jod"],"names":["JORDANIAN DINAR"]},{"symbol":"¥","codes":["jpy"],"names":["JAPANESE YEN"]},{"symbol":"Ksh","codes":["kes"],"names":["KENYAN SHILLING"]},{"symbol":"KHR","codes":["khr"],"names":["CAMBODIAN RIEL"]},{"symbol":"CF","codes":["kmf","xaf","xof"],"names":["COMORIAN FRANC","CFA FRANC BEAC","CFA FRANC BCEAO"]},{"symbol":"₩","codes":["krw"],"names":["SOUTH KOREAN WON"]},{"symbol":"KD","codes":["kwd","mkd"],"names":["KUWAITI DINAR","MACEDONIAN DENAR"]},{"symbol":"KZT","codes":["kzt"],"names":["KAZAKHSTANI TENGE"]},{"symbol":"L.L.","codes":["lbp"],"names":["LEBANESE POUND"]},{"symbol":"SLRs","codes":["lkr"],"names":["SRI LANKAN RUPEE"]},{"symbol":"Lt","codes":["ltl"],"names":["LITHUANIAN LITAS"]},{"symbol":"Ls","codes":["lvl"],"names":["LATVIAN LATS"]},{"symbol":"LD","codes":["lyd"],"names":["LIBYAN DINAR"]},{"symbol":"MAD","codes":["mad"],"names":["MOROCCAN DIRHAM"]},{"symbol":"MDL","codes":["mdl"],"names":["MOLDOVAN LEU"]},{"symbol":"MGA","codes":["mga"],"names":["MALAGASY ARIARY"]},{"symbol":"MKD","codes":["mkd"],"names":["MACEDONIAN DENAR"]},{"symbol":"MMK","codes":["mmk"],"names":["MYANMA KYAT"]},{"symbol":"MOP$","codes":["mop"],"names":["MACANESE PATACA"]},{"symbol":"MURs","codes":["mur"],"names":["MAURITIAN RUPEE"]},{"symbol":"MX$","codes":["mxn"],"names":["MEXICAN PESO"]},{"symbol":"RM","codes":["myr"],"names":["MALAYSIAN RINGGIT"]},{"symbol":"MTn","codes":["mzn"],"names":["MOZAMBICAN METICAL"]},{"symbol":"N$","codes":["nad"],"names":["NAMIBIAN DOLLAR"]},{"symbol":"₦","codes":["ngn"],"names":["NIGERIAN NAIRA"]},{"symbol":"C$","codes":["nio"],"names":["NICARAGUAN CÓRDOBA"]},{"symbol":"Nkr","codes":["nok"],"names":["NORWEGIAN KRONE"]},{"symbol":"NPRs","codes":["npr"],"names":["NEPALESE RUPEE"]},{"symbol":"NZ$","codes":["nzd"],"names":["NEW ZEALAND DOLLAR"]},{"symbol":"OMR","codes":["omr"],"names":["OMANI RIAL"]},{"symbol":"B/.","codes":["pab"],"names":["PANAMANIAN BALBOA"]},{"symbol":"S/.","codes":["pen"],"names":["PERUVIAN NUEVO SOL"]},{"symbol":"₱","codes":["php"],"names":["PHILIPPINE PESO"]},{"symbol":"PKRs","codes":["pkr"],"names":["PAKISTANI RUPEE"]},{"symbol":"zł","codes":["pln"],"names":["POLISH ZLOTY"]},{"symbol":"₲","codes":["pyg"],"names":["PARAGUAYAN GUARANI"]},{"symbol":"QR","codes":["qar"],"names":["QATARI RIAL"]},{"symbol":"RON","codes":["ron"],"names":["ROMANIAN LEU"]},{"symbol":"din.","codes":["rsd"],"names":["SERBIAN DINAR"]},{"symbol":"RUB","codes":["rub"],"names":["RUSSIAN RUBLE"]},{"symbol":"RWF","codes":["rwf"],"names":["RWANDAN FRANC"]},{"symbol":"SR","codes":["sar"],"names":["SAUDI RIYAL"]},{"symbol":"SDG","codes":["sdg"],"names":["SUDANESE POUND"]},{"symbol":"Skr","codes":["sek"],"names":["SWEDISH KRONA"]},{"symbol":"S$","codes":["sgd"],"names":["SINGAPORE DOLLAR"]},{"symbol":"Ssh","codes":["sos"],"names":["SOMALI SHILLING"]},{"symbol":"SY£","codes":["syp"],"names":["SYRIAN POUND"]},{"symbol":"฿","codes":["thb"],"names":["THAI BAHT"]},{"symbol":"DT","codes":["tnd"],"names":["TUNISIAN DINAR"]},{"symbol":"T$","codes":["top","ttd","twd"],"names":["TONGAN PAʻANGA","TRINIDAD AND TOBAGO DOLLAR","NEW TAIWAN DOLLAR"]},{"symbol":"TL","codes":["try"],"names":["TURKISH LIRA"]},{"symbol":"TT$","codes":["ttd"],"names":["TRINIDAD AND TOBAGO DOLLAR"]},{"symbol":"NT$","codes":["twd"],"names":["NEW TAIWAN DOLLAR"]},{"symbol":"TSh","codes":["tzs"],"names":["TANZANIAN SHILLING"]},{"symbol":"₴","codes":["uah"],"names":["UKRAINIAN HRYVNIA"]},{"symbol":"USh","codes":["ugx"],"names":["UGANDAN SHILLING"]},{"symbol":"$U","codes":["uyu"],"names":["URUGUAYAN PESO"]},{"symbol":"UZS","codes":["uzs"],"names":["UZBEKISTAN SOM"]},{"symbol":"Bs.F.","codes":["vef"],"names":["VENEZUELAN BOLÍVAR"]},{"symbol":"₫","codes":["vnd"],"names":["VIETNAMESE DONG"]},{"symbol":"FCFA","codes":["xaf"],"names":["CFA FRANC BEAC"]},{"symbol":"CFA","codes":["xof"],"names":["CFA FRANC BCEAO"]},{"symbol":"YR","codes":["yer"],"names":["YEMENI RIAL"]},{"symbol":"R","codes":["zar"],"names":["SOUTH AFRICAN RAND"]},{"symbol":"ZK","codes":["zmk"],"names":["ZAMBIAN KWACHA"]},{"symbol":"ZWL$","codes":["zwl"],"names":["ZIMBABWEAN DOLLAR"]}]
+
+function getTargetVal(val, cur, codes, names){
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+     var res = JSON.parse(this.responseText).inr;
+     var target = ((1/res[cur])*val).toFixed(2);//give option to user
+
+     document.getElementById("source").value = val;
+     document.getElementById("target").value = target;
+     var sD = document.getElementById("sourceSel");
+     if(!Array.isArray(codes)) sD.options[sD.options.length] = new Option(names, cur);
+     else{
+       for(var i = 0;i<codes.length;i++){
+         var opt = new Option(names[i], codes[i]);
+         sD.options[sD.options.length] = opt;
+       }
+     }
+     var tD = document.getElementById("targetSel");
+     tD.options[tD.options.length] = new Option("INDIAN RUPEE", "inr");
+
+
+  }
+  xhttp.open("GET", "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr.json");//showing old cached item
+  xhttp.send();
+}
+
+chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+}, function(selection) {
+  //const d = new Date();
+  //const dt = ""+d.getUTCFullYear()+"-"+d.getUTCMonth()+"-"+d.getUTCDate();
+  //document.getElementById("ad").innerHTML = dt;
+    var selected = selection[0].trim();
+    selected = selected.split(" ");
+
+    var val = 0.0, cur = "";
+    var invalid = false;
+
+    if(selected.length == 1){
+      var sel = selected[0];
+      if( !isNaN( parseInt( sel[0] ) ) ){//first char is num
+        if( isNaN( parseInt( sel[sel.length-3] ) ) && sel[sel.length-3] != '.' ){//3rd last is a char
+          cur = sel.substring( sel.length-3 );
+          val = parseFloat( sel.substring( 0, sel.length-3 ) );
+        }else{invalid = true;}
+      }else{//first char is not a num
+        if( !isNaN( parseInt( sel[1] ) ) ){//second is num
+          cur = sel[0];
+          val = parseFloat( sel.substring(1) );
+        }
+      }
+    }else{
+      //length 2
+      if(selected.length == 2){//either has to be a num
+
+        if( !isNaN( parseInt(selected[0]) ) ){//first is num
+          val = parseFloat(selected[0]);
+          cur = selected[1];
+        }else if( !isNaN( parseInt(selected[1]) ) ){//second is
+          val = parseFloat(selected[1]);
+          cur = selected[2];
+        }else{invalid = true;}//what if currency is appended to any?
+      }
+      else if(selected.length == 3){//middle has to be a num
+        if( !isNaN( parseInt(selected[1]) ) ){
+          //middle is numeric then take last is currency
+          val = parseFloat(selected[1]);
+          cur = selected[2];
+        }else{invalid = true;}//same issue as in len 2
+      }
+    }
+
+    var codes = cur;//incase of multiple cur
+    var names = cur;
+    if(!invalid){
+        if(cur.length == 1){
+          for(var i=0;i<symCodes.length;i++){
+            if(symCodes[i]["symbol"] == cur){
+
+              codes = symCodes[i]["codes"];
+              cur = symCodes[i]["codes"][0];
+              names = symCodes[i]["names"];
+            }
+          }
+        }else{
+          //get name of cur
+          var br = true;
+          for(var i =0;i<symCodes.length && br;i++){
+            var nms = symCodes[i]["names"];
+            var cds = symCodes[i]["codes"];
+            for(var j=0;j<nms.length;j++){
+              if(cds[j] == cur.toLowerCase()){
+                names = nms[j];
+                br = false;
+                break;
+              }
+            }
+          }
+        }
+
+      if(!isNaN(parseFloat(val)) && cur.length == 3){
+        getTargetVal(val, cur.toLowerCase(), codes, names);
+      }
+    }
+
+});
